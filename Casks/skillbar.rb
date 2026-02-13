28efa89d22cd4d6e38eb93d3483656ccfe111349
vnnkl <8235476+vnnkl@users.noreply.github.com>
2026-02-14 00:44:33 +0100

    chore: update cask SHA256 for v0.1.0

--- Casks/skillbar.rb
+++ Casks/skillbar.rb
@@ -1,6 +1,6 @@
 cask "skillbar" do
   version "0.1.0"
-  sha256 "PLACEHOLDER"
+  sha256 "b49cc6a99942dfdbd4e0d7273160ddc231e04a0099af3e52bbdda1cdffd03677"
 
   url "https://github.com/vnnkl/SkillBar/releases/download/v#{version}/SkillBar-#{version}.zip"
   name "SkillBar"
